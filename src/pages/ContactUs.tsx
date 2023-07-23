f6deeac5e8953e461422be75b34f017896d2b837
unknown <richardkrich19@gmail.com>
2023-07-23 21:18:28 +0700

    update

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,30 +1,20 @@
 import React from "react";
 import "../index.css";
 import "../fonts.css";
+import "../styles/ContactUs.css";
 function ContactUs() {
     return (
         <div id="wrapper">
-            <h1
-                style={{
-                    fontFamily: "brixton",
-                    position: "relative",
-                    top: "30vh"
-                }}
+            <h1 className="header-nuansa-contact"
             >
                 Contact Us!
             </h1>
-            <div
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    position: "relative",
-                    top: "30vh"
-                }}
-            >
-                <img src="/message.png" className="image-contact"></img>
-                <img src="/person.png" className="image-contact"></img>
-                <img src="/phone.png" className="image-contact"></img>
+            <div className="images">
+                <h2>
+                <img src="/message.png" className="message"></img>
+                <img src="/person.png" className="person"></img>
+                <img src="/phone.png" className="phone"></img>
+                </h2>
             </div>
         </div>
     );
--- src/pages/NUANSAHistory.tsx
+++ src/pages/NUANSAHistory.tsx
@@ -1,38 +1,25 @@
 import React from "react";
 import "../index.css";
 import "../fonts.css";
+import "../styles/NUANSAHistory.css";
 import { Link } from "react-router-dom";
 function NUANSAHistory() {
   return (
-    <div id="wrapper" style={{ fontFamily: "cursive" }}>
-      <h1
-        style={{
-          fontFamily: "aloja",
-          textAlign: "center"
-        }}
+    <div>
+      <h2 className= "header-nuansa-history"
       >
         NUANSA History
-      </h1>
+      </h2>
       <div style={{display: "flex"}}>
       <Link to="/NUANSA2018" rel="noreferrer">
         <img
-          src="/Greybox.png"
-          style={{
-            height: 400,
-            width: "40vw",
-            alignSelf: "left",
-            marginRight: "2vw"
-          }}
+          className="grey-box1"
+            src="/Greybox.png"
         ></img>
       </Link>
       <img
-        src="/Greybox.png"
-        style={{
-          height: 400,
-          width: "40vw",
-          alignSelf: "right",
-          marginLeft: "2vw"
-        }}
+          className="grey-box2"
+          src="/Greybox.png"
       ></img>
       </div>
     </div>
